005392fda8e7af21b739ac47dc0a8bce276871da
Takuya Murakami <tmurakam@tmurakam.org>
2020-01-05 22:29:53 +0900

    update: 12: use CoreDNS

--- scripts/k8s-the-hard-way/1210-verify-dns.sh
+++ scripts/k8s-the-hard-way/1210-verify-dns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl run busybox --image=busybox:1.28 --command -- sleep 3600
+kubectl run --generator=run-pod/v1 busybox --image=busybox:1.28 --command -- sleep 3600
 
 watch kubectl get pods -l run=busybox
