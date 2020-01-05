3a382e4a69539545cc9fbd8ff770a97cce52fc8e
Takuya Murakami <tmurakam@tmurakam.org>
2020-01-05 22:12:11 +0900

    update: api-server-certificate hosts

--- scripts/k8s-the-hard-way/0415-kubernetes-api-server-certificate.sh
+++ scripts/k8s-the-hard-way/0415-kubernetes-api-server-certificate.sh
@@ -2,6 +2,8 @@
 
 KUBERNETES_PUBLIC_ADDRESS=10.240.0.40
 
+KUBERNETES_HOSTNAMES=kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local
+
 cat > kubernetes-csr.json <<EOF
 {
   "CN": "kubernetes",
@@ -25,6 +27,6 @@ cfssl gencert \
   -ca=ca.pem \
   -ca-key=ca-key.pem \
   -config=ca-config.json \
-  -hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,kubernetes.default \
+  -hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,${KUBERNETES_HOSTNAMES} \
   -profile=kubernetes \
   kubernetes-csr.json | cfssljson -bare kubernetes
