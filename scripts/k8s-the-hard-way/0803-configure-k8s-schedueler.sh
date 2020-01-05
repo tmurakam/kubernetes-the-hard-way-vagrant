377278669c655844ceccb012d1e62e9ab59cfa00
Takuya Murakami <tmurakam@tmurakam.org>
2020-01-05 22:19:41 +0900

    update: 08: update control plane 1.10.2 -> 1.15.3

--- scripts/k8s-the-hard-way/0803-configure-k8s-schedueler.sh
+++ scripts/k8s-the-hard-way/0803-configure-k8s-schedueler.sh
@@ -3,7 +3,7 @@
 sudo mv kube-scheduler.kubeconfig /var/lib/kubernetes/
 
 cat <<EOF | sudo tee /etc/kubernetes/config/kube-scheduler.yaml
-apiVersion: componentconfig/v1alpha1
+apiVersion: kubescheduler.config.k8s.io/v1alpha1
 kind: KubeSchedulerConfiguration
 clientConnection:
   kubeconfig: "/var/lib/kubernetes/kube-scheduler.kubeconfig"
